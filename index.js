2577f332431a2c590143e94f7832901e3637519a
Danielsweet1 <sweetpalma9@gmail.com>
2022-10-25 20:41:16 +0600

    find data with id

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+const classes = require('./data/class.json')
+
 app.use(cors())
 const port =process.env.port || 5000
 
 app.get('/', (req, res)=>{
     res.send('server running')
+})
+
+app.get('/classes', (req, res)=>{
+    res.send(classes);
+})
+
+app.get('/classes/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+
+    const selected = classes.find(c=> c.id === id)
+    res.send(selected)
 })
 
 app.listen(port, ()=>{
